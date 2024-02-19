0f6050fddaf08c79bd91850d00c22f65de1bdd1f
alex8088 <244096523@qq.com>
2024-02-19 22:13:05 +0800

    chore(create-electron): update playground

--- packages/create-electron/playground/solid-ts/src/renderer/src/components/Versions.tsx
+++ packages/create-electron/playground/solid-ts/src/renderer/src/components/Versions.tsx
@@ -8,7 +8,6 @@ const Versions: Component = () => {
       <li class="electron-version">Electron v{versions().electron}</li>
       <li class="chrome-version">Chromium v{versions().chrome}</li>
       <li class="node-version">Node v{versions().node}</li>
-      <li class="v8-version">V8 v{versions().v8}</li>
     </ul>
   )
 }
