f83b0aba707c5a638bc932b3097af4563b3ce87c
远浅 <370732889@qq.com>
2024-03-29 19:32:10 +0800

    refactor: update preloader d.ts

--- packages/create-electron/template/solid-ts/src/preload/index.ts
+++ packages/create-electron/template/solid-ts/src/preload/index.ts
@@ -3,6 +3,7 @@ import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
 const api = {}
+export type api = typeof api
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
