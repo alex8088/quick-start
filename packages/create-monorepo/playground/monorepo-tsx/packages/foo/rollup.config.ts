124cbf3644f2d83f4e5c55dd26f7ee66ec9bc7f8
alex8088 <244096523@qq.com>
2024-04-02 21:51:42 +0800

    chore(create-monorepo): update playground

---
+++ packages/create-monorepo/playground/monorepo-tsx/packages/foo/rollup.config.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { createRequire } from 'node:module'
+import fs from 'node:fs/promises'
+import { defineConfig } from 'rollup'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import ts from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+
+function clean(when, target) {
+  const _clean = async (target) => {
+    await fs.rm(target, { recursive: true, force: true }).catch(() => {})
+  }
+  return {
+    name: 'clean',
+    buildStart: async () => {
+      if (when !== 'buildStart') return
+      await _clean(target)
+    },
+    buildEnd: async () => {
+      if (when !== 'buildEnd') return
+      await _clean(target)
+    }
+  }
+}
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      clean('buildStart', 'dist'),
+      resolve(),
+      commonjs(),
+      ts({
+        compilerOptions: {
+          rootDir: 'src',
+          declaration: true,
+          outDir: 'dist/types'
+        }
+      })
+    ]
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: pkg.types, format: 'es' }],
+    plugins: [dts(), clean('buildEnd', 'dist/types')]
+  }
+])
