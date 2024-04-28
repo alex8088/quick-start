d472f48eee7707a687c514e0849a8957616d7d12
Luke Edwards <lukeedwards30167116@gmail.com>
2024-04-28 22:36:25 +0100

    feat(create-electron): vue dev tools added

--- packages/create-electron/template/vue/electron.vite.config.mjs
+++ packages/create-electron/template/vue/electron.vite.config.mjs
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   main: {
@@ -15,6 +16,6 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [vue()]
+    plugins: [vue(), VueDevTools()]
   }
 })
