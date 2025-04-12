c6cf232b4c71dbb84b623d7ec20afdf29433775d
alex8088 <244096523@qq.com>
2025-04-12 21:32:07 +0800

    chore(create-electron): update eslint-plugin-vue to v10

--- packages/create-electron/template/vue/eslint.config.mjs
+++ packages/create-electron/template/vue/eslint.config.mjs
@@ -1,11 +1,24 @@
 import eslintConfig from '@electron-toolkit/eslint-config'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
 
 export default [
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   eslintConfig,
   ...eslintPluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        extraFileExtensions: ['.vue']
+      }
+    }
+  },
   {
     files: ['**/*.{js,jsx,vue}'],
     rules: {
