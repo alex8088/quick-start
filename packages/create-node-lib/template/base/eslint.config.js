b81c734818964fdabf9b3ebc28c3afeadc0ca48d
alex8088 <244096523@qq.com>
2025-04-13 20:39:05 +0800

    chore(create-node-lib): update eslint rules

--- packages/create-node-lib/template/base/eslint.config.js
+++ packages/create-node-lib/template/base/eslint.config.js
@@ -28,15 +28,35 @@ export default tseslint.config(
         'error',
         { 'ts-ignore': 'allow-with-description' }
       ],
-      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+          allowTypedFunctionExpressions: true,
+          allowHigherOrderFunctions: true,
+          allowIIFEs: true
+        }
+      ],
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-empty-function': [
         'error',
         { allow: ['arrowFunctions'] }
       ],
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        { allowInterfaces: 'always' }
+      ],
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/no-var-requires': 'off'
+      '@typescript-eslint/no-require-imports': 'error',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTaggedTemplates: true,
+          allowTernary: true
+        }
+      ]
     }
   },
   {
