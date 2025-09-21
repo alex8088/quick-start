66d1c0ff4d98e8b61a5a919f58f1ad88bf4f605b
alex8088 <244096523@qq.com>
2025-09-21 16:17:18 +0800

    fix(create-electron): use ESlint core's `defineConfig`

--- packages/create-electron/template/solid-ts/eslint.config.mjs
+++ packages/create-electron/template/solid-ts/eslint.config.mjs
@@ -1,8 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginSolid from 'eslint-plugin-solid'
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   tseslint.configs.recommended,
   eslintPluginSolid.configs['flat/typescript'],
--- packages/create-electron/template/svelte-ts/eslint.config.mjs
+++ packages/create-electron/template/svelte-ts/eslint.config.mjs
@@ -1,8 +1,9 @@
+import { defineConfig } from 'eslint/config'
 import tseslint from '@electron-toolkit/eslint-config-ts'
 import eslintConfigPrettier from '@electron-toolkit/eslint-config-prettier'
 import eslintPluginSvelte from 'eslint-plugin-svelte'
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['**/node_modules', '**/dist', '**/out'] },
   tseslint.configs.recommended,
   eslintPluginSvelte.configs['flat/recommended'],
